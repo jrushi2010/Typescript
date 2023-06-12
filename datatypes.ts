c7c1e427f6c188cbf57b2446311676e66b20e394
jrushi2010 <jrushi2010@gmail.com>
2023-06-12 11:59:16 +0530

    datatypes, functions,class,array

--- datatypes.ts
+++ datatypes.ts
@@ -1,3 +1,89 @@
-let lname = 'john';
+let lname : string;
+
+lname = 'john';
+
+let newname = lname.toUpperCase();
+
+console.log(newname);
+
+let age:number;
+
+age = 25;
+age = 25.4;
+
+let dob = "25";
+
+let result = parseInt(dob);
+
+console.log(dob);
+
+
+let isValid : boolean = false;
+
+console.log(isValid);
+
+let empList : string[];
+
+empList = ["santosh","ganesh"];
+
+let numlist : Array<number>;
+
+numlist = [1,2,3,4,5]
+
+let result1 = numlist.filter((num)=> num > 2);
+
+let emp = empList.find((emp)=>emp==="santosh");
+
+let sum = numlist.reduce((acc, num)=>acc + num);
+
+console.log(emp);
+
+console.log(result1);
+
+console.log(sum);
+
+const enum Color {
+    Red,
+    Green,
+    Blue
+}
+
+let c:Color = Color.Blue;
+
+let swapNum: [firstNumber: number,secondNumber:number];
+
+function swapNumbers(num1:number,num2:number) : [number,number]{
+    return [num2,num1]
+}
+
+swapNum = swapNumbers(10,20);
+swapNum[0];
+swapNum[1];
+
+let department : any;
+
+department = "IT";
+department = "csc";
+department = 10;
+
+console.log(department);
+
+function add(num1:number,num2:number){
+    return num1 + num2;
+}
+
+let newsum = add(10,20);
+console.log(newsum);
 
 //lname = 10;
+/* 
+string
+number
+boolean
+array
+enum
+tuple
+any
+void
+never
+*/
---
+++ functions.ts
@@ -0,0 +1,40 @@
+import { Type } from "typescript";
+
+function add1(num1:number,num2:number, num3?:number): number{
+    return num3 ? num1 + num2 + num3 : num1 + num2;
+}
+
+console.log(add1(2,3));
+
+console.log(add1(2,3,5));
+
+
+const sub = (num1:number, num2:number, num3=10): number => num1 - num2 - num3;
+
+console.log(sub(2,3));
+
+console.log(sub(2,3,5));
+
+
+const mult = function(num1:number,num2:number):number{
+    return num1 * num2;
+}
+
+
+function add2(num1:number,num2:number,...num3:number[]):number{
+    return num1 + num2 + num3.reduce((a,b)=> a + b, 0);
+}
+
+let numbers = [1,2,3,4,5]
+console.log(add2(2,3,...numbers));
+console.log(add2(2,3,5,6,7));
+
+
+function getItem<Type>(item:Type[]):Type[]{
+    return new Array<Type>().concat(item);
+}
+
+let concatResult = getItem<number>([1,2,3,4,5]);
+
+let concatString = getItem<string>(["a","b","c","d","e"]);
+
